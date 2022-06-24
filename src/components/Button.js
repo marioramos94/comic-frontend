00149cab6bcce964d263e3a8573c0b6cbea432f7
Mario Ramos <mario.ramos@zervizgroup.com>
2022-06-24 13:04:35 -0400

    css improvements

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Button({ disabled, onClick, text }) {
   return (
-    <div
+    <button
       className={disabled ? "pagination-button-disabled" : "pagination-button"}
       disabled={disabled}
       onClick={() => onClick()}
     >
       {text}
-    </div>
+    </button>
   );
 }
 
